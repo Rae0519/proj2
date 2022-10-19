072d210be92f22c4ef92b64d33cda73caa582fb3
Ella1823 <74345123+Sihan1823@users.noreply.github.com>
2022-10-19 16:09:24 +0100

    new

--- pract2.R
+++ pract2.R
@@ -1,69 +1,70 @@
-Pone <- function(n,k,strategy,nreps){
-  box <- 1:(2*n)
-  count <- 10000
-  # strategy 1
-  if (strategy == 1){
-    for (i in 1:nreps) {
-      box <- sample(box,2*n)
-      num <- k
-      t <- n
-      while (t > 0){
-        num <- box[num]
-        if (num == k){break}
-        t = t - 1
-      }
-      if (t == 0){
-        count = count - 1
-      }
-    }
-  }
-  
-  # strategy 2
-  if (strategy == 2){
-    for (i in 1:nreps) {
-      box <- sample(box,2*n)
-      num <- sample(box,1)
-      t <- n
-      while (t > 0){
-        num = box[num]
-        if (num == k){break}
-        t = t - 1
-      }
-      if (t == 0){
-        count = count - 1
-      }
-    }
-  }
-  # strategy 3
-  if (strategy == 3){
-    for (i in 1:nreps) {
-      box <- sample(box,n)
-      if (k %in% box[] == T){
-        break
-      }
-      else {
-        count = count -1
-      }
-      }
-  }
-  
-  prob <- count/nreps
-  return(prob)
-  }
-  
-Pall <- function(n,strategy,nreps){
-  prob <- 0
-  for (i in 1:(2*n)) {
-    k <- sample(1:(2*n),1)
-    prob = prob + Pone(n,k,strategy,nreps)
-  }
-  prob <- prob/(2*n)
-  return(prob)
-}
-  
 
-nrep <- 10000
-k <- 4
-n <- 50
-Pone(50,1,1,10000)
-Pall(50,1,10000)
+success <- function(n, k, p, strategy) {
+  success = 0
+  escape = rep(0, p)
+  card = sample(1:(2 * n))
+  if (strategy == 1) {
+    for (i in 1:p) {
+      boxes = 0
+      box_start = k
+      while (boxes < n && !escape[i]) {
+        boxes = boxes + 1
+        escape[i] = card[box_start] == k
+        box_start = card[box_start]
+      }
+    }
+  }
+  
+  else if (strategy == 2) {
+    for (i in 1:p) {
+      boxes = 0
+      box_start = sample(card,1)
+      while (boxes < n && !escape[i]) {
+        boxes = boxes + 1
+        escape[i] = card[box_start] == k
+        box_start = card[box_start]
+      }
+      
+    }
+  }
+  
+  else if (strategy == 3) {
+    for (i in 1:p) {
+      escape[i] = i %in% sample(card, n)
+    }
+    
+  }
+  success = sum(escape) == p
+  success
+}
+
+
+Pone <- function(n,k,strategy,nreps = 10000){
+  success = 0
+  for (i in 1:nreps){
+    success = success + success(n,k,1,strategy)
+  }
+  success/nreps
+}
+
+Pall <- function(n,strategy,nreps = 10000){
+  success = 0
+  for (i in 1:nreps){
+    k <- sample(1:(2*n),1)
+    success = success + success(n,k,2*n,strategy)
+  }
+  success/nreps
+}
+
+
+# Pall(5,3)
+
+# Pall(50,1)
+
+
+
+
+
+
+
+
