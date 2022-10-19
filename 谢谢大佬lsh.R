18f9aee46529c1188be9955d00cf66e821c6f56e
LifuZheng23148 <114320357+LifuZheng23148@users.noreply.github.com>
2022-10-19 17:38:57 +0100

    Add files via upload

---
+++ 谢谢大佬lsh.R
@@ -0,0 +1,74 @@
+
+success <- function(n, p, strategy) {
+  k = sample(1:(2*n))
+  success = 0
+  escape = rep(0, p)
+  card = sample(1:(2 * n))
+  if (strategy == 1) {
+    for (i in 1:p) {
+      boxes = 0
+      box_start = k[i]
+      while (boxes < n && !escape[i]) {
+        boxes = boxes + 1
+        escape[i] = card[box_start] == k[i]
+        box_start = card[box_start]
+      }
+    }
+  }
+  
+  else if (strategy == 2) {
+    for (i in 1:p) {
+      boxes = 0
+      box_start = sample(card,1)
+      while (boxes < n && !escape[i]) {
+        boxes = boxes + 1
+        escape[i] = card[box_start] == k[i]
+        box_start = card[box_start]
+      }
+      
+    }
+  }
+  
+  else if (strategy == 3) {
+    for (i in 1:p) {
+      escape[i] = i %in% sample(card, n)
+    }
+    
+  }
+  if (sum(escape) == p){
+     success + 1
+  }
+  else {
+    success
+  }
+}
+
+
+Pone <- function(n,k,strategy,nreps = 10000){
+  successtimes = 0
+  for (i in 1:nreps){
+    successtimes = successtimes + success(n,1,strategy)
+  }
+  successtimes/nreps
+}
+
+Pall <- function(n,strategy,nreps = 10000){
+  allsuccess = 0
+  for (i in 1:nreps){
+    allsuccess = allsuccess + success(n,2*n,strategy)
+  }
+  allsuccess/nreps
+}
+
+
+# Pall(5,3)
+
+# Pall(50,1)
+
+
+
+
+
+
+
+
